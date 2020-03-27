a5bc4010758acb416ca5462004a54c127f705f06
Wenfeng Song <vincentsong2008@gmail.com>
2020-03-27 23:00:21 +1100

    fixed pytest error

--- dynamic_programming/leetcode/test_leetcode_dp.py
+++ dynamic_programming/leetcode/test_leetcode_dp.py
@@ -1,11 +1,12 @@
-from dp_53_maximum_subarray import Solution53
-from dp_70_climbing_stairs import Solution70
+from .dp_53_maximum_subarray import Solution53
+from .dp_70_climbing_stairs import Solution70
 
-class Test_LeetCode_DPs:
+
+class TestLeetCodeDPs:
 
     def test_max_sub_array(self):
         nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-        assert Solution53().maxSubArray(nums) == 6
+        assert Solution53.max_sub_array(nums) == 6
 
     def test_climb_stairs(self):
-        assert Solution70().climbStairs(5) == 8
+        assert Solution70.climb_stairs(5) == 8
